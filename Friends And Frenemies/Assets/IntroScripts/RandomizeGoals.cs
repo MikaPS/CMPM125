ae9334522fe2fa704aff4fa0acbaeb78a9dd3bf2
MikaPS <mikaps7@gmail.com>
2023-11-14 15:06:15 -0800

    Basic main menu + randomize goals

---
+++ Friends And Frenemies/Assets/IntroScripts/RandomizeGoals.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class RandomizeGoals : MonoBehaviour
+{
+    private List<string> goalsArray = new List<string>  { "Pet a dog", "Collect 10 wood", "Destroy a building", "Talk to 3 NPCs", "End the zombie apocalypse"};
+    public Text goalsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Selects a random value from the goals array, removes it, and returns it
+   public string getRandomGoal() {
+    if (goalsArray.Count > 0) {
+        int i = Random.Range(0, goalsArray.Count);
+        string goal = goalsArray[i];
+        goalsArray.RemoveAt(i);
+        return goal;
+    }
+    return "";
+   }
+
+   public void setTextToGoals() {
+        goalsText.text = "\n" + getRandomGoal() + "\n" + getRandomGoal();
+   }
+}
