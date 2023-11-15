245cb3abea181a0b91b700b6f1fceb4318e6cca6
MikaPS <mikaps7@gmail.com>
2023-11-14 23:40:50 -0800

    Added counter + a line to split screen

--- Friends And Frenemies/Assets/Scripts/Collectable.cs
+++ Friends And Frenemies/Assets/Scripts/Collectable.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Collectable : MonoBehaviour
 {
-
+    public Text woodCount;
+    private static int count = 0; // Static since all wood objects have the script
     public void Start()
     {
+
     }
 
     void OnTriggerEnter2D(Collider2D collider)
@@ -14,6 +17,8 @@ public class Collectable : MonoBehaviour
         if(collider.gameObject.tag == "Player1" || collider.gameObject.tag == "Player2")
         {
             gameObject.SetActive(false);
+            count += 1;
+            woodCount.text = "Wood: " + count;
         }
     }
 }
