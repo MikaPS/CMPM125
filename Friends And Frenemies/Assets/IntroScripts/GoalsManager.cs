2352cefe044e5edc8925e85a0f2808dd430ab130
MikaPS <mikaps7@gmail.com>
2023-11-23 23:10:24 -0800

    refactored goals

--- Friends And Frenemies/Assets/IntroScripts/GoalsManager.cs
+++ Friends And Frenemies/Assets/IntroScripts/GoalsManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public struct Goal
+{
+    public int targetValue;
+    public string type;
+
+    public Goal(int targetValue, string type)
+    {
+        this.targetValue = targetValue;
+        this.type = type;
+    }
+}
 
 public class GoalsManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GoalsManager GoalManager;
-    public List<string> player1Goals = new List<string>{};
+    public List<Goal> player1Goals = new List<Goal>{};
     // public string[] player1Goal2;
-    public List<string> player2Goals = new List<string>{};
+    public List<Goal> player2Goals = new List<Goal>{};
     // public string[] player2Goal2;
 
     void Awake()
@@ -31,4 +44,18 @@ public class GoalsManager : MonoBehaviour
         
     }
     
+    public int CheckPlayerGoals(List<Goal> playerGoals, int stoneCount, int woodCount)
+    {
+        int playerGoalsCompleted = 0;
+        for (int i = 0; i < playerGoals.Count; i += 1)
+        {
+            string resourceType = playerGoals[i].type;
+            int targetAmount = playerGoals[i].targetValue;
+            if ((resourceType == "Stone" && stoneCount >= targetAmount) || (resourceType == "Wood" && woodCount >= targetAmount))
+            {
+                playerGoalsCompleted += 1;
+            }
+        }
+        return playerGoalsCompleted;
+    }
 }
