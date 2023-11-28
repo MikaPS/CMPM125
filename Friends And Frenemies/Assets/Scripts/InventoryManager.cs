eefc6db5b3ce02f6e77a24f0b68c8b4c7f8d37fb
Justin Vicente <javicent@ucsc.edu>
2023-11-28 11:40:42 -0800

    Inventory Manager

---
+++ Friends And Frenemies/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class InventoryManager : MonoBehaviour
+{
+    public enum ResourceType
+    {
+        Wood,
+        Stone,
+        Water,
+        Food,
+        RawFood
+    }
+
+    [System.Serializable]
+    public class Resource
+    {
+        public ResourceType type;
+        public int amount;
+    }
+
+    public List<Resource> resources = new List<Resource>();
+
+    public void AddResource(ResourceType type, int amount)
+    {
+        Resource existingResource = resources.Find(r => r.type == type);
+
+        if (existingResource != null)
+        {
+            existingResource.amount += amount;
+        }
+        else
+        {
+            Resource newResource = new Resource { type = type, amount = amount };
+            resources.Add(newResource);
+        }
+
+    }
+
+    public void RemoveResource(ResourceType type, int amount)
+    {
+        Resource existingResource = resources.Find(r => r.type == type);
+
+        if (existingResource != null)
+        {
+            existingResource.amount -= amount;
+
+            if (existingResource.amount <= 0)
+            {
+                resources.Remove(existingResource);
+            }
+
+        }
+        else
+        {
+            Debug.LogWarning("Attempted to remove non-existent resource: " + type);
+        }
+    }
+
+    public void PrintInventory()
+    {
+        foreach (Resource resource in resources)
+        {
+            Debug.Log(resource.type + ": " + resource.amount);
+        }
+    }
+
+    void Start()
+    {
+    }
+}
