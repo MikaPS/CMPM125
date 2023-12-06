ac112bb922bcf2a3b746a8238b11d460e4a0c813
MikaPS <mikaps7@gmail.com>
2023-12-05 21:29:55 -0800

    added some juice

--- Friends And Frenemies/Assets/Scripts/Collectable.cs
+++ Friends And Frenemies/Assets/Scripts/Collectable.cs
@@ -5,20 +5,6 @@ using UnityEngine.UI;
 
 public class Collectable : MonoBehaviour
 {
-    // public Text countText;
-    // public Text winText;
-
-    // private static int woodCount = 0; // Static since all wood objects have the script
-    // private static int stoneCount = 0; // Static since all wood objects have the script
-
-    // public int player1GoalsCompleted = 0;
-    // public int player2GoalsCompleted = 0;
-  
-    public void Start()
-    {
-
-    }
-
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.gameObject.tag == "Player1" || collider.gameObject.tag == "Player2")
@@ -41,13 +27,6 @@ public class Collectable : MonoBehaviour
                 // countText.text = "" + woodCount;
             }
             Destroy(gameObject);
-
-            // if (GoalsManager.GoalManager.CheckPlayerGoals(GoalsManager.GoalManager.player1Goals) == 2) {
-            // winText.text = "PLAYER 1 WON!!";
-            // }
-            // if (GoalsManager.GoalManager.CheckPlayerGoals(GoalsManager.GoalManager.player2Goals) == 2) {
-            //     winText.text = "PLAYER 2 WON!!";
-            // }
         }
     }
 }
--- Friends And Frenemies/Assets/Scripts/ToxicGarden/TextNextToPlayer.cs
+++ Friends And Frenemies/Assets/Scripts/ToxicGarden/TextNextToPlayer.cs
@@ -10,14 +10,16 @@ public class TextNextToPlayer : MonoBehaviour
     public Transform effects1;
     public Transform effects2;
     public string currentPlayer = "";
-    // public Sprite sprite;
     public float yOffset = 0f;  // Offset in the y-axis above the player
     public float xOffset = 20f;  
     public SpriteRenderer spriteRenderer1;
-    
     public SpriteRenderer spriteRenderer2;
+    public ParticleSystem particleSystem1;
+    public ParticleSystem particleSystem2;
     void Awake()
     {
+        particleSystem1.Stop(); 
+        particleSystem2.Stop(); 
         // gameObject.SetActive(!gameObject.activeSelf);
         if (textEffects == null)
         {
@@ -30,46 +32,40 @@ public class TextNextToPlayer : MonoBehaviour
         }
     }
     void start() {
-        // spriteRenderer = GetComponent<SpriteRenderer>();
+        
     }
 
     public void updateSprite(string currentPlayer, Sprite sprite)
     {  
-        // spriteRenderer.enabled = true;
-        // spriteRenderer.sprite = sprite;
-        // spriteRenderer.sortingOrder = 999;
-        // Vector3 playerPosition = player1.position;
         if (currentPlayer == "Player2") {
+            Vector3 playerPosition = player2.position;
+            Vector3 abovePlayerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y + yOffset, 1);
+            particleSystem2.transform.position = abovePlayerPosition;
+            particleSystem2.Play();
             spriteRenderer2.enabled = true;
             spriteRenderer2.sprite = sprite;
             spriteRenderer2.sortingOrder = 999;
-            Vector3 playerPosition = player2.position;
-            Vector3 abovePlayerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y + yOffset, 1);
             effects2.position = abovePlayerPosition;
-            Invoke("changeVisibility2", 2.0f);
+            Invoke("changeVisibility2", 1.0f);
         } else {
+            Vector3 playerPosition = player1.position;
+            Vector3 abovePlayerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y + yOffset, 1);
+            particleSystem1.transform.position = abovePlayerPosition;
+            particleSystem1.Play();
             spriteRenderer1.enabled = true;
             spriteRenderer1.sprite = sprite;
             spriteRenderer1.sortingOrder = 999;
-            Vector3 playerPosition = player1.position;
-            Vector3 abovePlayerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y + yOffset, 1);
             effects1.position = abovePlayerPosition;
-            Invoke("changeVisibility1", 2.0f);
+            Invoke("changeVisibility1", 1.0f);
         }
-        // Vector3 abovePlayerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y + yOffset, 1);
-        // transform.position = abovePlayerPosition;   
-        // if (currentPlayer == "Player2") {
-        //     effects2 = abovePlayerPosition;
-        // } else {
-        //     effects1 = abovePlayerPosition;
-        // }
-        // Invoke("changeVisibility", 2.0f);
     }
 
     public void changeVisibility2() {
         spriteRenderer2.enabled = !spriteRenderer2.enabled;
+        particleSystem2.Stop();
     }
     public void changeVisibility1() {
         spriteRenderer1.enabled = !spriteRenderer1.enabled;
+        particleSystem1.Stop();
     }
 }
